7c0e13baa1796f352efd81c6850eb83a39960b8a
jefferson <jeff.otoni@gmail.com>
2020-06-26 10:57:25 -0300

    update grpc

--- grpc/client/main.go
+++ grpc/client/main.go
@@ -14,11 +14,11 @@ type Client struct {
 	Timeout time.Duration
 }
 
-func (c *Client) Work(job *engdb_pb.Job) (res *engdb_pb.Result, err error) {
+func (c *Client) Work(job *engdb_pb.Job) (res *engdb_pb.Reply, err error) {
 	conn, err := ggrpc.Dial(c.Host, ggrpc.WithInsecure())
 	fmt.Printf("\nNew connection %v, timeout[%s]", c.Host, c.Timeout)
 	if err != nil {
-		fmt.Errorf("Error dial gRPC Worker client \n error[%v]", err)
+		fmt.Printf("Error dial gRPC Worker client \n error[%v]", err)
 		return nil, err
 	}
 	defer conn.Close()
@@ -29,9 +29,9 @@ func (c *Client) Work(job *engdb_pb.Job) (res *engdb_pb.Result, err error) {
 	defer cancel()
 
 	w := engdb_pb.NewWorkerServiceClient(conn)
-	res, err = w.Perform(ctx, job)
+	res, err = w.Get(ctx, job)
 	if err != nil {
-		fmt.Errorf("Error performing gRPC work error[%v] \n job[%v]", err, job)
+		fmt.Printf("Error performing gRPC work error[%v] \n job[%v]", err, job)
 		return res, err
 	}
 	return res, nil
@@ -46,16 +46,5 @@ func createJob() *engdb_pb.Job {
 	job := &engdb_pb.Job{}
 	job.Name = "Job Eng..2020..."
 	job.Id = "1234696969624696xx"
-	job.Params = []byte(`{"x": "42"}`)
-
-	headers := make(map[string]string)
-	headers["key1"] = "value1"
-	headers["key2"] = "value2"
-	job.Headers = headers
-
-	configs := make(map[string]string)
-	configs["etc1"] = "value1"
-	configs["etc2"] = "value2"
-	job.Configs = configs
 	return job
 }
